3a4af06953da1085ed3fc0dca40ff52c339d3efc
Karim Jair Servin <93849158+KarimServin@users.noreply.github.com>
2022-06-03 17:41:16 -0300

    Add files via upload

---
+++ src/components/nabvar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const NavbarFunction = () => {
+   
+   return <div className='containerDiv'>
+
+       <div className='tituloDiv'>La Libreria del Tecnico</div>
+    
+   <nav>   
+         <a href="#">Inicio</a>
+         <a href="#">Productos</a>
+         <a href="#">Contacto</a>
+         <a href="#">Carrito</a>
+    </nav>
+              
+          </div>
+
+}
